1943ab2c6cbf41ebc5ce8efc3bb7b6c025154e9d
dipadeka <dipadeka13@gmail.com>
2026-02-04 12:53:04 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+/*import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'*/
+import './App.css'
+import LoginForm from './LoginForm'
+
+function App() {
+  
+  return (
+    <>
+      <div>
+        <h1>Welcome!</h1>
+        <LoginForm/>
+      </div>
+      
+    </>
+  )
+
+}
+export default App
+
+
---
+++ src/LoginForm.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+
+export default function LoginForm() {
+     const [isLogin, setIsLogin] = useState(false);
+     return (
+          <div className="container">
+               <div className="form-container">
+                    <div className="form-toggle">
+                         <button type="button" className={isLogin ? 'active' : ""} onClick={() => setIsLogin(true)}>Login</button>
+                         <button type="button" className={!isLogin ? 'active' : ""} onClick={() => setIsLogin(false)}>Signup</button>
+                    </div>
+                    {isLogin ? <>
+                         <div className='form'>
+                              <h2>Login Form</h2>
+                              <input type='email' placeholder='Email' />
+                              <input type='password' placeholder="Password" />
+                              <a href='#'>Forgot Password</a>
+                              <button>Login</button>
+                              <p>Not a Member? <a href='#' onClick={() => setIsLogin(false)}>Signup Now</a></p>
+                         </div>
+                    </> : <>
+                         <div className='form'>
+                              <h2>Signup Form</h2>
+                              <input type='email' placeholder='Email' />
+                              <input type='password' placeholder='Password' />
+                              <input type='password' placeholder='Confirm Password' />
+                              <button>Signup</button>
+                         </div>
+                    </> }
+               </div> 
+          </div>
+
+     );
+
+}
+
